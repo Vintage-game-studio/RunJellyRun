a380122ddaa37dd9bc568e405086f0bbbdd66268
Arash Bahrehmand <stdmwmrule@gmail.com>
2018-04-01 22:22:09 +0200

    Draw in Editor!

---
+++ RunJellyRun/Assets/Editor/ProjectileDrawer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(ProjectileCalculation))]
+public class ProjectileDrawer : Editor
+{
+    private Material material;
+    private List<Vector3> linePairs;
+    private ProjectileCalculation _projectileCalculation;
+    private bool isDrawing = false;
+    
+    void OnEnable()
+    {
+        // Find the "Hidden/Internal-Colored" shader, and cache it for use.
+        material = new Material(Shader.Find("Hidden/Internal-Colored"));
+        linePairs = new List<Vector3>();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        _projectileCalculation = (ProjectileCalculation) target;
+        
+        DrawDefaultInspector();
+        Rect layoutRectangle = GUILayoutUtility.GetRect(10, 10000, 200, 200);
+        if (GUILayout.Button("Draw Projectile"))
+        {
+            linePairs = _projectileCalculation.GetProjectilePoints(layoutRectangle.width,layoutRectangle.height);
+            isDrawing = true;
+        }
+        
+        GUILayout.BeginHorizontal(EditorStyles.helpBox);
+
+        if (Event.current.type == EventType.Repaint)
+        {
+            GUI.BeginClip(layoutRectangle);
+            GL.PushMatrix();
+
+            GL.Clear(true, false, Color.black);
+            material.SetPass(0);
+
+            GL.Begin(GL.QUADS);
+            GL.Color(Color.black);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(layoutRectangle.width, 0, 0);
+            GL.Vertex3(layoutRectangle.width, layoutRectangle.height, 0);
+            GL.Vertex3(0, layoutRectangle.height, 0);
+            GL.End();
+
+            if (isDrawing)
+            {
+                GL.Color(new Color(0.5f, 0.5f, 0.9f));
+                GL.Begin(GL.LINE_STRIP);   
+                foreach (Vector3 point in linePairs)
+                {
+                    GL.Vertex3(layoutRectangle.width/4+point.x, -point.y+layoutRectangle.height, point.z);
+                }
+                GL.End();                
+            }
+
+            GL.PopMatrix();
+            GUI.EndClip();
+        }
+
+        // End our horizontal 
+        GUILayout.EndHorizontal();
+    }
+}
--- RunJellyRun/Assets/Scripts/Tools/ProjectileUtility/ProjectileCalculation.cs
+++ RunJellyRun/Assets/Scripts/Tools/ProjectileUtility/ProjectileCalculation.cs
@@ -2,8 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Timeline;
-using Vuforia;
+
 
 public class ProjectileCalculation: MonoBehaviour
 {
@@ -25,54 +24,109 @@ public class ProjectileCalculation: MonoBehaviour
   private float deltaX1;
   private float deltaX2;
   private float g1,g2;
-  private float velocity1, velocity2;
-  private float t1, t2, addTime;
+  private float velocity;
   private bool second = false;
-  Vector2 GetPosition(float t)
+  private Vector2 beforRotVector2;
+  private Vector2 rotationPivot;
+  private float maxWidth;
+  private float maxHeight;
+
+  public List<KeyValuePair<Vector3,Vector3>> GetLines(float width, float height)
+  {
+    List<KeyValuePair<Vector3,Vector3>> linePairs_Norm=new List<KeyValuePair<Vector3, Vector3>>();
+    foreach (KeyValuePair<Vector3,Vector3> keyValuePair in linePairs)
+    {
+      Vector3 key=new Vector3(width*keyValuePair.Key.x/maxWidth,height*keyValuePair.Key.y/maxHeight,0);
+      Vector3 value=new Vector3(width*keyValuePair.Value.x/maxWidth,height*keyValuePair.Value.y/maxHeight,0);
+      linePairs_Norm.Add(new KeyValuePair<Vector3, Vector3>(key,value));
+      //Debug.Log(new KeyValuePair<Vector3, Vector3>(width*keyValuePair.Key/maxWidth,height*keyValuePair.Value/maxHeight));
+    }
+    
+    return linePairs_Norm;
+  }
+  
+  Vector2 GetPosition(float t,float gravity)
   { 
     float px = InitialVelocity * Mathf.Cos(Mathf.Deg2Rad * InitialAngle) * t + this.initialPosition2D.x;
-    float py = 0.5f*g * Mathf.Pow(t, 2) + InitialVelocity * Mathf.Sin(Mathf.Deg2Rad * InitialAngle) * t +this.initialPosition2D.y;
+    float py = 0.5f*gravity * Mathf.Pow(t, 2) + InitialVelocity * Mathf.Sin(Mathf.Deg2Rad * InitialAngle) * t +this.initialPosition2D.y;
     
-    return RotatePointAroundPivot(new Vector2(px, py), this.initialPosition2D, new Vector3(0, 0, rotAngle));
+    return new Vector2(px,py);
   }
 
   void Start()
   {
-    this.initialPosition2D = new Vector2(this.transform.position.x,this.transform.position.y);
-    Vector2 disVec = new Vector2(TargetGO.transform.position.x,TargetGO.transform.position.y) - this.initialPosition2D;
-    this.rotAngle = Mathf.Acos(Vector2.Dot(disVec.normalized, Vector2.right))*Mathf.Rad2Deg;
 
-    float dis = Vector3.Distance(TargetGO.transform.position, this.transform.position);
-    deltaX1 = (dis * ratio)*2;
-    deltaX2 = (dis - deltaX1/2)*2;
-    
-    float halfExDuration = ((Duration*ratio)/2);
-    addTime = Duration * (1 - ratio) / 2; 
-    
-    Debug.Log("ddis: "+ dis);
-    Debug.Log("deltaX1: "+ deltaX1);
-    Debug.Log("deltaX2: "+ deltaX2);
-    
-    g1=CalculateGravity(deltaX1,Duration*ratio*2,this.InitialAngle);        
-    g2=CalculateGravity(deltaX2,Duration * (1 - ratio),this.InitialAngle*(1+ratio));
-
-    velocity1 = CalculateVelocity(deltaX1, Duration*ratio*2,this.InitialAngle);
-    velocity2 = CalculateVelocity(deltaX2,Duration * (1 - ratio),this.InitialAngle*(1+ratio));
-
-    g = g1;
-    this.InitialVelocity = velocity1;
   }
 
+  public List<Vector3> GetProjectilePoints(float width, float height)
+  {
+    float timeInterval;
+    List<Vector3> projectilePoints=new List<Vector3>();
+    Vector3 currentPos = Vector3.zero;
+    float gravity = 0;
+    second = false;
+    
+    float dis = Vector3.Distance(TargetGO.transform.position, this.transform.position);
+    deltaX1 = (dis * ratio)*2;
+    deltaX2 = (dis - deltaX1/2)*2;    
+
+    gravity = CalculateGravity(deltaX1,Duration*ratio*2,this.InitialAngle);   
+    this.InitialVelocity = CalculateVelocity(deltaX1, Duration*ratio*2,this.InitialAngle);      
+    this.initialPosition2D = new Vector2(this.transform.position.x,this.transform.position.y);
+    Vector2 diffVec2 = new Vector2(TargetGO.transform.position.x,TargetGO.transform.position.y) - this.initialPosition2D;
+    this.rotAngle = -Mathf.Acos(Vector2.Dot(diffVec2.normalized, Vector2.right)) * Mathf.Rad2Deg;
+    this.rotationPivot = this.initialPosition2D;
+
+    timeInterval = this.Duration / 100f;
+    float spentTime = 0;
+    float overalTime=0;
+    maxHeight=  0.5f*gravity * Mathf.Pow(Duration*ratio, 2) + this.InitialVelocity * Mathf.Sin(Mathf.Deg2Rad * InitialAngle) * this.Duration*ratio +this.initialPosition2D.y;
+    maxWidth= 2*dis;
+    
+    while (Math.Abs(spentTime - Duration) > timeInterval && Vector2.Distance(currentPos, TargetGO.transform.position) > 0.1)
+    {
+      currentPos = GetPosition(spentTime,gravity);
+      Vector2 normCurrentPos=new Vector2(width*currentPos.x/maxWidth,height*currentPos.y/maxHeight);
+      Vector2 rotatedVec2= RotatePointAroundPivot(normCurrentPos, this.rotationPivot, new Vector3(0, 0, rotAngle));
+      projectilePoints.Add(rotatedVec2);
+      
+      if (Math.Abs(spentTime - Duration * ratio) < 0.01 && !second)
+      {
+        this.initialPosition2D.x += ((deltaX1 / 2) - deltaX2/2);       
+        gravity = -8 * (currentPos.y-this.initialPosition2D.y) / (float)Mathf.Pow((Duration * (1 - ratio) * 2), 2);
+        this.InitialAngle = Mathf.Rad2Deg * Mathf.Atan(4 * (currentPos.y-this.initialPosition2D.y) / deltaX2);
+        this.InitialVelocity = (-gravity * Duration * (1 - ratio)*2)/(2*Mathf.Sin(Mathf.Deg2Rad * this.InitialAngle));
+        spentTime = Duration * (1 - ratio);
+       
+        second = true;        
+      }
+      spentTime += timeInterval;
+      overalTime += timeInterval;
+      Debug.Log(overalTime);
+      Debug.Log("dis: "+Vector2.Distance(currentPos, TargetGO.transform.position));
+    }
+    return projectilePoints;
+  }
+  
   void Update()
   {
     if (Input.GetKey(KeyCode.Space) && !jump)
     {
+      float dis = Vector3.Distance(TargetGO.transform.position, this.transform.position);
+      deltaX1 = (dis * ratio)*2;
+      deltaX2 = (dis - deltaX1/2)*2;    
+
+      g = CalculateGravity(deltaX1,Duration*ratio*2,this.InitialAngle);
+    
+      this.InitialVelocity = CalculateVelocity(deltaX1, Duration*ratio*2,this.InitialAngle);
+      
       this.initialPosition2D = new Vector2(this.transform.position.x,this.transform.position.y);
       Vector2 diffVec2 = new Vector2(TargetGO.transform.position.x,TargetGO.transform.position.y) - this.initialPosition2D;
-      this.rotAngle = Mathf.Acos(Vector2.Dot(diffVec2.normalized, Vector2.right)) * Mathf.Rad2Deg;
+      this.rotAngle = -Mathf.Acos(Vector2.Dot(diffVec2.normalized, Vector2.right)) * Mathf.Rad2Deg;
       time = 0;
-
+      this.rotationPivot = this.initialPosition2D;
       jump = true;
+      second = false;
     }
   }
 
@@ -86,48 +140,36 @@ public class ProjectileCalculation: MonoBehaviour
     return deltaX / (Mathf.Cos(angle *Mathf.Deg2Rad) * duration);
   }
   
+  
   void FixedUpdate()
   {
     if (jump)
     {
       if (Math.Abs(time - Duration) < Time.deltaTime ||
-          Vector2.Distance(transform.position, TargetGO.transform.position) < 0.01)
+          Vector2.Distance(transform.position, TargetGO.transform.position) < 0.1)
       {
         jump = false;
         //linePairs.Clear();
         return;
       }
-
-      t1 = time;
       if (Math.Abs(time - Duration*ratio) < 0.01 && !second)
       {
-        //Vector2 preInitPos2D = this.initialPosition2D;
         this.initialPosition2D.x += ((deltaX1 / 2) - deltaX2/2);
-        //this.initialPosition2D = RotatePointAroundPivot(this.initialPosition2D,preInitPos2D,new Vector3(0, 0, rotAngle));
         
-        GameObject go= GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        go.transform.position = this.initialPosition2D;
-        
-        Debug.Log("this.initialPosition2D.x: "+ this.initialPosition2D.x);
-        g = -8 * this.transform.position.y / (float)Mathf.Pow((Duration * (1 - ratio) * 2), 2);
-        Debug.Log("g: "+ g);
-        this.InitialAngle = Mathf.Rad2Deg * Mathf.Atan(4 * this.transform.position.y / deltaX2);
-        Debug.Log("this.InitialAngle: "+this.InitialAngle);
+        g = -8 * (this.beforRotVector2.y-this.initialPosition2D.y) / (float)Mathf.Pow((Duration * (1 - ratio) * 2), 2);
+        this.InitialAngle = Mathf.Rad2Deg * Mathf.Atan(4 * (this.beforRotVector2.y-this.initialPosition2D.y) / deltaX2);
         this.InitialVelocity = (-g*Duration * (1 - ratio)*2)/(2*Mathf.Sin(Mathf.Deg2Rad*this.InitialAngle));
-        Debug.Log("this.InitialVelocity: "+this.InitialVelocity);
-        
+
         time = Duration * (1 - ratio);
        
-        second = true;
-        
+        second = true;        
       }
       
       Vector3 prevPos = this.transform.position;
-      this.transform.position = GetPosition(time);
+      //this.transform.position = GetPosition(time);
       linePairs.Add(new KeyValuePair<Vector3, Vector3>(prevPos,this.transform.position));
       
       time += Time.fixedDeltaTime;
-      Debug.Log(time);
     }
   }
 
@@ -149,4 +191,5 @@ public class ProjectileCalculation: MonoBehaviour
   public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Vector3 angles) {
     return Quaternion.Euler(angles) * (point - pivot) + pivot;
   }
+  
 }
