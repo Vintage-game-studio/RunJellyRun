ba57c1ee098b3e7b248d17ceb17e00f01d606d85
Arash Bahrehmand <stdmwmrule@gmail.com>
2018-04-02 19:44:45 +0200

    Adding Constructors to Projectile.

--- RunJellyRun/Assets/Scripts/Tools/ProjectileUtility/Projectile.cs
+++ RunJellyRun/Assets/Scripts/Tools/ProjectileUtility/Projectile.cs
@@ -1,16 +1,102 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour {
+public class Projectile 
+{
+	public struct ProjectileData_gravity_duration
+	{
+		public Transform TargetTransform;
+		public float Gravity;
+		public float Duration;
+	}
 
-	// Use this for initialization
-	void Start () {
-		
+	public struct ProjectileData_initialVelocity_duration
+	{
+		public Transform TargetTransform;
+		public float InitialVelocity;
+		public float Duration;
+	}
+
+	public struct ProjectileData_height_duration
+	{
+		public Transform TargetTransform;
+		public float Height;
+		public float Duration;
+	}
+
+	public struct Projectile_initialAngle_gravity_initVelocity
+	{
+		public float InitVelocity;
+		public float InitAngle;
+		public float Gravity;
+	}
+
+
+	public Transform TargetTransform;
+	public Transform InitialTransform;
+	public float InitialVelocity;
+	public float Gravity;
+	public float Height;
+	public float InitialAngle;
+	public float Duration;
+	public float Ratio;
+
+	private Vector2 currentPosiion;
+
+	public Projectile(Transform initialTransform, float ratio,ProjectileData_initialVelocity_duration projectileDataInitialVelocityDuration)
+	{
+		this.InitialTransform = initialTransform;
+		this.TargetTransform = projectileDataInitialVelocityDuration.TargetTransform;
+		this.InitialVelocity = projectileDataInitialVelocityDuration.InitialVelocity;
+		this.Duration = projectileDataInitialVelocityDuration.Duration;
+		this.Ratio = ratio;
+	}
+
+	public Projectile(Transform initialTransform,float ratio,ProjectileData_height_duration projectileDataHeightDuration)
+	{
+		this.InitialTransform = initialTransform;
+		this.TargetTransform = projectileDataHeightDuration.TargetTransform;
+		this.Height = projectileDataHeightDuration.Height;
+		this.Duration = projectileDataHeightDuration.Duration;
+		this.Ratio = ratio;
+	}
+
+	public Projectile(Transform initialTransform,float ratio,ProjectileData_gravity_duration projectileDataGravityDuration)
+	{
+		this.InitialTransform = initialTransform;
+		this.TargetTransform = projectileDataGravityDuration.TargetTransform;
+		this.Gravity = projectileDataGravityDuration.Gravity;
+		this.Duration = projectileDataGravityDuration.Duration;
+		this.Ratio = ratio;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	public Projectile(Transform initialTransform,float ratio,Projectile_initialAngle_gravity_initVelocity projectileInitialAngleGravityInitVelocity)
+	{
+		this.InitialTransform = initialTransform;
+		this.InitialVelocity = projectileInitialAngleGravityInitVelocity.InitVelocity;
+		this.Gravity = projectileInitialAngleGravityInitVelocity.Gravity;
+		this.InitialAngle = projectileInitialAngleGravityInitVelocity.InitAngle;
+		this.Ratio = ratio;
+	}	
+
+	float GetRotateAngle()
+	{
 		
+		return 0;
 	}
+
+	public Vector2 GetCurrentPosition(float time)
+	{
+		float px = this.InitialVelocity * Mathf.Cos(Mathf.Deg2Rad * this.InitialAngle) * time + this.InitialTransform.position.x;
+		float py = 0.5f * this.Gravity * Mathf.Pow(time, 2) + this.InitialVelocity * Mathf.Sin(Mathf.Deg2Rad * this.InitialAngle) * time +this.InitialTransform.position.y;
+    
+		currentPosiion=new Vector2(px,py);
+		
+		return currentPosiion;
+	}
+	
+
 }
